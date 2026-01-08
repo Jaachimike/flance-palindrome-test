db4f9d87f2ef8d79ae250aac5aea6ced5ac7ce81
Jaachi Okafor <okaforjaachi@gmail.com>
2026-01-08 01:10:53 +0100

    Add VCS configuration and PalindromeChecker class implementation

--- PalindromeChecker.java
+++ PalindromeChecker.java
@@ -1,7 +1,6 @@
 public class PalindromeChecker {
     public boolean isPalindrome(int x) {
-        // Negative numbers are not palindromes
-        // Numbers ending in 0 (except 0 itself) are not palindromes
+        // Negative numbers are not palindromes 
         if (x < 0 || (x % 10 == 0 && x != 0)) {
             return false;
         }
